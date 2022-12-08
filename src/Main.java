700fe164dc14d03423c9de7ef34ae23c7b9f3b2f
Chris <cr1s@hotmail.it>
2022-12-08 10:22:26 +0100

    Add: Exercise If 2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String mySurname = "Puglisi";
+
+        if ( mySurname.contains("ni") == true){
+            System.out.println("Your surname contain the sequence of letters 'ni' ");
+        }else{
+            System.out.println("Your surname don't contain the sequence of letters 'ni'");
+        }
+    }
+}
